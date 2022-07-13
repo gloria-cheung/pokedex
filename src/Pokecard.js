050707e6947bca8cce8a5f69a74fd5c254844194
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-12 20:02:07 -0400

    add func to create img and implement jsx logic

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,6 +1,20 @@
 
-export default function Pokecard() {
+export default function Pokecard(props) {
+  const {id, name, type, base_experience} = props;
+  const formatURL = (id) => {
+    if (id < 10) {
+      id = "00" + id;
+    } else if (id >= 10 && id <100) {
+      id = "0" + id;
+    }
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`
+  }
   return (
-    <h1>Pokecard</h1>
+    <div>
+      <h1>{name}</h1>
+      <img src={formatURL(id)}/>
+      <p>Type: {type}</p>
+      <p>EXP: {base_experience}</p>
+    </div>
   );
 }
