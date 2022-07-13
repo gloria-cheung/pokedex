1ab6873fb74acc7fdac922e32a321294058f1973
gloria <macuser@MacUsers-MacBook-Pro.local>
2022-07-12 21:23:24 -0400

    wrap img in div for styling

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,6 +1,5 @@
 import './Pokecard.css'
 
-
 export default function Pokecard(props) {
   const {id, name, type, base_experience} = props;
   const formatURL = (id) => {
@@ -10,11 +9,14 @@ export default function Pokecard(props) {
       id = "0" + id;
     }
     return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${id}.png`
-  }
+  };
+  
   return (
     <div className="Pokecard">
       <h2 className="Pokecard-title">{name}</h2>
-      <img src={formatURL(id)} alt={name}/>
+      <div className="Pokecard-img">
+        <img src={formatURL(id)} alt={name}/>
+      </div>
       <div className="Pokecard-data">Type: {type}</div>
       <div className="Pokecard-data">EXP: {base_experience}</div> 
     </div>
